f307682ddcee054b44c69a846940a3fa70993aba
Nicolás Palermo <50157861+niklauspalerme@users.noreply.github.com>
2021-06-30 15:08:22 -0300

    Update Content.jsx

--- src/componentes/Content.jsx
+++ src/componentes/Content.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 
+
+const Part = ({name,excercises}) =>{
+
+    console.log(name);
+    console.log(excercises);
+
+    return <li>{name} {excercises}</li>
+
+}
+
 const Content =({parts})=>{
+
+    console.log(parts)
 
     return(
         <>
             <ul>
                 {
-                    parts.map( part => <li key={part.id}>{part.name} {part.excersises}</li>)
+                 parts.map( part => <Part key={part.id} name= {part.name} excercises= {part.exercises}/>)
                 }
             </ul>
         </>
